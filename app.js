5deb99ad237e86ba36a66998dd7731efea46b9a0
LemoineSimon <lemoine.simon.perso@gmail.com>
2018-11-08 20:15:27 +0100

    Express load static files

--- app.js
+++ app.js
@@ -4,38 +4,38 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 server.listen(8080);
 
-var Players = require( __dirname + '/module/Core/Players');
-var Player = require( __dirname + '/module/Model/Player');
+var Players = require(__dirname + '/module/Core/Players');
+var Player = require(__dirname + '/module/Model/Player');
 
 var players = new Players();
 
 var rooms = [];
 
-app.use(express.static('public'));
+app.use('/dist', express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function (socket) {
-	var player = new Player( socket );
-	players.addPlayer( player );	
+    var player = new Player(socket);
+    players.addPlayer(player);
 
 
-	socket.on('init',function(){
-		
-	});
+    socket.on('init', function () {
 
-	socket.on('disconnect', function () {
-		player = players.getPlayer( player );
-		players.removePlayer( player );
-		socket.broadcast.emit('disconnect player', player );
-	});
+    });
 
-	loadEvents( socket );
+    socket.on('disconnect', function () {
+        player = players.getPlayer(player);
+        players.removePlayer(player);
+        socket.broadcast.emit('disconnect player', player);
+    });
+
+    loadEvents(socket);
 });
 
 
-function loadEvents( socket ){
-	
+function loadEvents(socket) {
+
 }
