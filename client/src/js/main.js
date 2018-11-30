5ccbe4f0946503136fd7b2b253500f6ede6afc1d
LemoineSimon <lemoine.simon.perso@gmail.com>
2018-11-30 21:16:37 +0100

    Front Mobile

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -3,10 +3,7 @@ import Phaser from 'phaser';
 
 import BootScene from './scenes/Boot'
 import GameScene from './scenes/Game'
-
 import Socket from './Socket'
-import MobileControls from "./MobileControls";
-
 import config from './config';
 import spells from './spells/spells';
 
@@ -28,11 +25,4 @@ class Game extends Phaser.Game {
     }
 }
 
-if (document.getElementById('game')) {
-    window.game = new Game();
-} else {
-    let socket = new Socket(playerInformation);
-
-    window.controls = new MobileControls(socket, spells.spells);
-}
-
+window.game = new Game();
